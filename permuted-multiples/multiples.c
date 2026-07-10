042c6e87a1c86eee91d2b8a993ac4b4951d7d5f8
Patrick Seute <patrick.seute@cox.net>
2026-07-10 15:31:28 -0400

    optimize by removing string conversion

--- permuted-multiples/multiples.c
+++ permuted-multiples/multiples.c
@@ -1,32 +1,35 @@
 #include <limits.h>
 #include <stdio.h>
-#include <string.h>
 
-#define BUFFSIZE 32
-
-void count_digits(const char *str, int c1[])
+// Fills counts with the number of times the index appears as a digit in value
+// Returns the number of digits int value
+int count_digits(unsigned int value, int counts[])
 {
-    for (size_t i = 0; i < strlen(str); i++)
+    if (value == 0)
     {
-        size_t current_digit = str[i] - '0';
-        c1[current_digit]++;
+        counts[0]++;
+        return 1;
     }
+    int count = 0;
+    while (value > 0)
+    {
+        const size_t digit = value % 10;
+        counts[digit]++;
+        value /= 10;
+        count++;
+    }
+    return count;
 }
 
 // Return 1 if the two integers contain the same exact digits
 int compare_digits(unsigned int d1, unsigned int d2)
 {
-    char b1[BUFFSIZE];
-    char b2[BUFFSIZE];
-    snprintf(b1, sizeof(b1), "%llu", d1);
-    snprintf(b2, sizeof(b2), "%llu", d2);
-
-    if (strlen(b1) != strlen(b2))
-        return 0;
     int c1[10] = {0};
     int c2[10] = {0};
-    count_digits(b1, c1);
-    count_digits(b2, c2);
+    int len1 = count_digits(d1, c1);
+    int len2 = count_digits(d2, c2);
+    if (len1 != len2)
+        return 0;
     for (size_t i = 0; i < 10; i++)
     {
         if (c1[i] != c2[i])
